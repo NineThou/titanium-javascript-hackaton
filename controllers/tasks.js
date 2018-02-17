edf8635a98ea1a626bdaa2bf03cf21f814b47da7
Vasilii <vasilii@V-Efros.local>
2018-02-17 16:00:29 +0200

    add task controller

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,15 +1,15 @@
 const Tasks = require('../models/Tasks');
 
-const Schema = mongoose.Schema;
 
 exports.getTask = async (req, res) => {
     try {
         const task = await Tasks.findById(req.params.id).exec(( err, taskById) => {
             return taskById
         })
-        res.render('/lessons.pug', {task: task}) 
+        res.render('task', {task: task}) 
     } catch(error){
         res.status(404).send("Ощибка при получение таска")
     }
-
 }
+
+
