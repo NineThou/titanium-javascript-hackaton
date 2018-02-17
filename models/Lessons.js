5bf4a854ce74e9bb4d2a2392f0aa4d614757483e
Vasilii <vasilii@V-Efros.local>
2018-02-17 11:32:12 +0200

    add Lesson schema

---
+++ models/Lessons.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var LessonsSchema = new Schema (
+    {
+        name: {type: String, required: true},
+        description: {type: String, required: true},
+        author: {type: Schema.ObjectId, ref: 'User'}
+    };
+);
+
+
+
+
+module.exports = mongoose.model('Lesson', LessonsSchema);
